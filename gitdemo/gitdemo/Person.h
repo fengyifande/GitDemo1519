9ce8f8a678d87d0c8c8c573e0180d78ce8fe8500
fyfan <1425277538@qq.com>
2016-02-26 14:20:19 +0800

    worker1 创建 person类

---
+++ gitdemo/gitdemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  gitdemo
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 冯逸帆. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ gitdemo/gitdemo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  gitdemo
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 冯逸帆. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
