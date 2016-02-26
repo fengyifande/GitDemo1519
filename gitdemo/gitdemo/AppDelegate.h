5a9fa72f0918cce4fbaf7aa0cebe9de5d6a3340f
fyfan <1425277538@qq.com>
2016-02-26 14:07:48 +0800

    first project

---
+++ gitdemo/gitdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitdemo
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 冯逸帆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
